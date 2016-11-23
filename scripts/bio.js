fb75e9e3ce357fb5350caa00e2856a9f7b119b0f
Noblet MANON <nmanon@e1r8p18.42.fr>
2016-11-23 16:12:20 +0100

    js bio

---
+++ scripts/bio.js
@@ -0,0 +1,7 @@
+var bio_precision = document.getElementById("bio_precision");
+
+function display_bio(bio)
+{
+	if (bio == "Lorem")
+		bio_precision.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}
