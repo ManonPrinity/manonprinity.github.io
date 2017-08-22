cae850bf10a1e4e83e8dc485bb6eb3b40d0f228c
Artemis <artemix@protonmail.com>
2017-08-22 07:59:05 +0200

    Cleaned a bit of js and few mistakes

--- scripts/skill.js
+++ scripts/skill.js
@@ -2,33 +2,46 @@ var skill_precision = document.getElementById("skill_precision");
 
 function display_skill(skill)
 {
-	if (skill == "html5")
-		skill_precision.innerHTML = "Connaissances bonnes. J'ai de bonnes pratiques en HTML5, et de bonnes connaissances en SEO."
-	else if (skill == "css3")
-		skill_precision.innerHTML = "Connaissances bonnes. Sélecteurs, pseudos-sélecteurs, sélecteurs avancés, animations, media queries..."
-	else if (skill == "javascript")
-		skill_precision.innerHTML = "Connaissances bonnes. Le Javascript est tout simplement le langage que je préfere !"
-	else if (skill == "angular")
-		skill_precision.innerHTML = "Connaissances moyennes d'AngularJS. J'ai decouvert ce framework il y a quelques mois. C'est aujourd'hui mon framework MVC préféré."
-	else if (skill == "nodejs")
-		skill_precision.innerHTML = "Connaissances moyennes. NodeJS est pour moi une redécouverte du développement Back End !"
-	else if (skill == "mongodb")
-		skill_precision.innerHTML = "Connaissances moyennes. Pour moi, MongoDB est dans une majorité des cas la base de données la plus appropriée pour NodeJS."
-	else if (skill == "php")
-		skill_precision.innerHTML = "Connaissances moyennes. J'ai appris le PHP, mais ce n'est pas mon langage préféré..."
-	else if (skill == "photoshop")
-		skill_precision.innerHTML = "Connaissances appliquées au web design."
-	else if (skill == "cordova")
-		skill_precision.innerHTML = "Connaissances moyennes. Competences égales avec Phonegap et Ionic."
-	else if (skill == "jquery")
-		skill_precision.innerHTML = "Connaissances moyennes. Un plus pour le javascript."
-	else if (skill == "sql")
-		skill_precision.innerHTML = "Connaissances moyennes. Complement BDD interessant avec PHP."
-	else if (skill == "C")
-		skill_precision.innerHTML = "Connaissances de base. En cours d'apprentissage."
-}
-
-function hide_skill()
-{
-	skill_precision.innerHTML = "Survolez mes competences pour plus d'informations";
+    let v;
+    switch (skill) {
+        case "html5":
+            v = "Connaissances bonnes. J'ai de bonnes pratiques en HTML5, et de bonnes connaissances en SEO.";
+            break;
+        case "css3":
+            v = "Connaissances bonnes. Sélecteurs, pseudos-sélecteurs, sélecteurs avancés, animations, media queries...";
+            break;
+        case "javascript":
+            v = "Connaissances bonnes. Le Javascript est tout simplement le langage que je préfère !";
+            break;
+        case "angular":
+            v = "Connaissances moyennes d'AngularJS. J'ai découvert ce framework il y a quelques mois. C'est aujourd'hui mon framework MVC préféré.";
+            break;
+        case "nodejs":
+            v = "Connaissances moyennes. NodeJS est pour moi une redécouverte du développement Back End !";
+            break;
+        case "mongodb":
+            v = "Connaissances moyennes. Pour moi, MongoDB est dans une majorité des cas la base de données la plus appropriée pour NodeJS.";
+            break;
+        case "php":
+            v = "Connaissances moyennes. J'ai appris le PHP, mais ce n'est pas mon langage préféré...";
+            break;
+        case "photoshop":
+            v = "Connaissances appliquées au web design.";
+            break;
+        case "cordova":
+            v = "Connaissances moyennes. Compétences égales avec Phonegap et Ionic.";
+            break;
+        case "jquery":
+            v = "Connaissances moyennes. Un plus pour le javascript.";
+            break;
+        case "sql":
+            v = "Connaissances moyennes. Complément BDD intéressant avec PHP.";
+            break;
+        case "C":
+            v = "Connaissances de base. En cours d'apprentissage.";
+            break;
+        default:
+            v = "Survolez mes compétences pour plus d'informations";
+    }
+    skill_precision.innerHTML = v;
 }
